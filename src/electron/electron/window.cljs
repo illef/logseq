2ba7530fceaf692eb3984add804120e9a3f05e0c
illef <sai.illef@gmail.com>
2025-09-12 15:43:43 +0900

    fix: 윈도우 포커스 설정 로직 수정
    - 윈도우 포커스 설정 시 불필요한 `setVisibleOnAllWorkspaces` 호출 제거
    - 관련 Electron 이슈(#8734) 참조 코드 삭제

--- src/electron/electron/window.cljs
+++ src/electron/electron/window.cljs
@@ -113,10 +113,7 @@
   [^js win]
   (when (.isMinimized ^object win)
     (.restore win))
-  ;; Ref: https://github.com/electron/electron/issues/8734
-  (.setVisibleOnAllWorkspaces win true)
-  (.focus win)
-  (.setVisibleOnAllWorkspaces win false))
+  (.focus win))
 
 (defn get-graph-all-windows
   [graph-path] ;; graph-path == dir
